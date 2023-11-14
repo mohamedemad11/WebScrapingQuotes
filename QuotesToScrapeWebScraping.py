220804dc89e5e676f1cedb672e36a406ebcdbac5
mohamedemad11 <mohamedemad353@gmail.com>
2023-11-14 03:05:01 +0200

    Added Scraping Authors

--- QuotesToScrapeWebScraping.py
+++ QuotesToScrapeWebScraping.py
@@ -16,8 +16,22 @@ with open('cleaned_quotes.txt' ,'w') as file :
             cleaned_first_quotation_mark= cleaning_apostroph.replace('”','')
             cleaned_text= cleaned_first_quotation_mark.replace('“','')
 
-            file.write(cleaned_text) 
+            file.write(cleaned_text.strip()) 
             file.write('\n')        
             
+with open('cleaned_authors.txt','w')   as f: 
+     for line in response.text.split('\n'):
+        if '<span>by <small class="author" ' in line :
         
+            cleaned_left_place=line.replace('<span>by <small class="author" itemprop="author">','')
+            cleaned_right_place=cleaned_left_place.replace('</small>','')
+        
+
+            f.write(cleaned_right_place.strip())
+            f.write('\n')
+
+
+            
+        
+
 
